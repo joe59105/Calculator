0993cd9915adca25e6a430c7e398f472c9e77660
habit <ayomidebakare301@gmail.com>
2023-03-09 08:48:41 +0100

    Updated to prevent automatic day increment

--- 4_19_Sales_Calculator.c
+++ 4_19_Sales_Calculator.c
@@ -18,36 +18,41 @@ int main()
     while (day < 8)
     {
         printf("\nDAY %d\n", day);
-        printf("%s", "Enter product number, quantity sold (-1 to quit): ");
+        printf("%s", "Enter product number, quantity sold (-1 to quit) (99 to move to next day): \n");
+        printf("Day %d \n", day);
         scanf("%d, %d", &productNumber, &quantitySold);
 
         // if the user wants to exit the program
         if (productNumber == -1)
         {
             break;
+        } else if(productNumber == 99){
+            day++;
+            continue;
         }
+
         // Using the switch cases to represent different prices, then multiply the price selected with the quantity sold then increment the sales variable accordingly
         switch (productNumber)
         {
         case 1:
             sales += (PRICE1 * quantitySold);
-            day++;
+            puts("Input received. Continue...");
             break;
         case 2:
             sales += (PRICE2 * quantitySold);
-            day++;
+            puts("Input received. Continue...");
             break;
         case 3:
             sales += (PRICE3 * quantitySold);
-            day++;
+            puts("Input received. Continue...");
             break;
         case 4:
             sales += (PRICE4 * quantitySold);
-            day++;
+            puts("Input received. Continue...");
             break;
         case 5:
             sales += (PRICE5 * quantitySold);
-            day++;
+            puts("Input received. Continue...");
             break;
         default:
             printf("%s", "Enter a valid product number between 1 and 5!\n\n");
