54296d7e35bcc507b27829486bf0a7c37fa7aa41
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-03-14 21:11:21 +0100

    feat:added a statement to the printf

--- 5_15_Hypotenuse_Calculations.c
+++ 5_15_Hypotenuse_Calculations.c
@@ -8,17 +8,14 @@ int main()
     double adjacent;
     double opposite;
 
-    // printf("%s", 'enter the opposite side of the triangle');
-    // printf('enter the opposite side of the triangle');
     puts("enter the opposite side of the triangle");
     scanf("%lf", &opposite);
 
-    // printf("%s", 'enter the adjacent side of the triangle');
-    // printf('enter the adjacent side of the triangle');
+    
     puts("enter the adjacent side of the triangle");
     scanf("%lf", &adjacent);
 
-    printf("%.2f", hypotenuse(adjacent, opposite));
+    printf("The hypotenuse of the triangle is $%lf", hypotenuse(adjacent, opposite));
 }
 
 double hypotenuse(double adjacent, double hypotenuse)
