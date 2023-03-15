d7f81805848950a8c427551cf1a5a6aa5f18fa6e
habit <ayomidebakare301@gmail.com>
2023-03-15 01:14:36 +0100

    Updated the code to fit the requirement of the latest version of the exercise

--- 5_19_Square_Gen.c
+++ 5_19_Square_Gen.c
@@ -1,33 +1,41 @@
 #include <stdio.h>
 
-void generateSquare(int length);
-void generator(int length, int depth);
+int getInput(char prompt[]);
+void generateRectangle(int length, int depth);
 
 int main(void){
     int prefLength;
-    puts("Type in a number between 1 and 50.");
+    int side1 = getInput("Type in a number for the first side (between 1 and 50)");
+    int side2 = getInput("Type in a number for the second side (between 1 and 50)");
+
+    generateRectangle(side1, side2);
+}
+
+int getInput(char prompt[]){
+    int prefLength=0;
+    puts(prompt);
 
     scanf("%d", &prefLength);
+    while(1){
     if(prefLength < 1 || prefLength > 50 ){
         puts("Invalid number! Please type in a number between 1 and 50.");
+        scanf("%d", &prefLength);
     } else {
-        generateSquare(prefLength);
+      return prefLength;
+    }
     }
 }
 
-void generateSquare(int length){
-   generator(length, length);
-}
 
-void generator(int length, int depth){
-    char patternString[length + 1];
+void generateRectangle(int side1, int side2){
+    char patternString[side1 + 1];
     //build the row
-    for(int i = 0; i < length; i++){
+    for(int i = 0; i < side1; i++){
         patternString[i] = '*';
     }
     //print the row
     printf("%s\n", patternString);
-    if(depth > 1){
-        generator(length, depth -1); //build the next row
+    if(side2 > 1){
+        generateRectangle(side1, side2-1); //build the next row
     }
 }
