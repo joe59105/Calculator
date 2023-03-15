df55654e2f7aa94350117c7eb6be4209efec239b
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-03-16 00:50:32 +0100

    added code

---
+++ 5_13_Random_number_generator.c
@@ -0,0 +1,36 @@
+#include <stdio.h>;
+
+
+int main(void) {
+        int lower = 1 , upper = 6;
+        srand(time(0));
+        int num = (rand() % (upper - lower + 1)) + lower;
+        printf("%d",num);
+  
+        int lower = 1 , upper = 6;
+        srand(time(0));
+        int num = (rand() % (upper - lower + 1)) + lower;
+        printf("%d",num);
+  
+        int lower = 1 , upper = 6;
+        srand(time(0));
+        int num = (rand() % (upper - lower + 1)) + lower;
+        printf("%d",num);
+  
+        int lower = 1 , upper = 6;
+        srand(time(0));
+        int num = (rand() % (upper - lower + 1)) + lower;
+        printf("%d",num);
+  
+        int lower = 1 , upper = 6;
+        srand(time(0));
+        int num = (rand() % (upper - lower + 1)) + lower;
+        printf("%d",num);
+  
+        int lower = 1 , upper = 6;
+        srand(time(0));
+        int num = (rand() % (upper - lower + 1)) + lower;
+        printf("%d",num);
+  
+    return 0;
+}
