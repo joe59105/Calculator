8e87e6debcef47455048f0d531447ab3600c41f0
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-03-16 22:34:04 +0100

    chore:added comments to 5_18_even_or_odd.c

--- 5_18_even_or_odd.c
+++ 5_18_even_or_odd.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// Function prototypes for the functions that check whether a number is even or odd
 bool is_even(int number) ;
 bool is_odd(int number) ;
 
@@ -9,9 +10,11 @@ int main(void)
 {
     int number;
 
+        // This loop keeps asking the user to enter a number until they enter -1
     while (1) {
         printf("enter a number (Press -1 to exit):");
         scanf("%d",&number);
+        // If the user enters -1, the loop is terminated
         if (number == -1)
         {
             break;
@@ -23,6 +26,7 @@ int main(void)
             printf("number is odd\n");
         }
     }
+    // The program returns 0 to indicate successful execution
     return 0;
 }
 
