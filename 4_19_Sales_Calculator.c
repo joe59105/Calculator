04f84e0cc179a3c219f71ddda354d3616cb1c784
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-03-16 22:04:51 +0100

    feat:added changes

--- 4_19_Sales_Calculator.c
+++ 4_19_Sales_Calculator.c
@@ -8,7 +8,8 @@ int main()
     const float PRICE3 = 9.98;
     const float PRICE4 = 4.49;
     const float PRICE5 = 6.87;
-
+    
+    //Declare other variables with their respective data types 
     int productNumber;
     int quantitySold;
     int day = 1;
@@ -17,22 +18,27 @@ int main()
     // We are going to make the loop iterate for a span of seven days , that is one week
     while (day < 8)
     {
-        int currentProductPrice = 0;
+        //Initialize the currentProductPrice to be zero 
+        int currentProductPrice = 0; 
         printf("\nDAY %d\n", day);
         printf("%s", "Enter product number, quantity sold (-1 to quit) (99 to move to next day): \n");
         printf("Day %d \n", day);
+        // We use the scanf function to receive input for the productNumber and the quantitySold
         scanf("%d, %d", &productNumber, &quantitySold);
 
-        // if the user wants to exit the program
+        // if the user wants to exit the program input -1
         if (productNumber == -1)
         {
             break;
-        } else if(productNumber == 99){
+        } 
+        //If the user wants to advance to the next day input 99
+        else if(productNumber == 99){
             day++;
             continue;
         }
 
-       //get product price
+       //get product price 
+       // Here we use a switch statement that receives input the number (that is the case in the switch statement) corresponds to the price of a particular product 
         switch (productNumber)
         {
         case 1:
@@ -55,7 +61,9 @@ int main()
             break;
         }
 
-        //Add these sales to the sales counter
+        //Add these sales to the sales counter 
+        // we first have an if check to see if the current product price is not 0 
+        // then use the add assignment operator to add the currentProductPrice multiplied by the quantitySold
         if(currentProductPrice != 0){
             sales+= currentProductPrice * quantitySold;
             puts("Input received. Continue...");
--- 5_11_Rounding_Numbers.c
+++ 5_11_Rounding_Numbers.c
@@ -1,5 +1,6 @@
-#include <stdio.h>;
-#include <math.h>;
+#include <stdio.h>
+#include <math.h>
+
 int roundToInteger(double number);
 double roundToTenths(double number);
 double roundToHundredths(double number);
